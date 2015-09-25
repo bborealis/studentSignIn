7d6f0b35660c5982c4d3357f5a3471e2b7eee6a6
Brian Borealis <brian.borealis@gmail.com>
2015-09-25 11:00:36 -0700

    add sign in and out button shows and hides

--- controllers/AttendancesController.js
+++ controllers/AttendancesController.js
@@ -1,9 +1,11 @@
 studentSignIn.controller('AttendancesCtrl', function AttendanceCtrl($scope, StudentsFactory) {
   $scope.students = StudentsFactory.students;
   $scope.addStudentPresent = function(student) {
-    student.present = true;
+    student.present = "true";
+    console.log(student.name + " present = " + student.present);
   };
   $scope.addStudentNotPresent = function(student) {
     student.present = false;
+    console.log(student.name + " present = " + student.present);
   };
 });
--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,8 +1,8 @@
 studentSignIn.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
-  factory.students = [{ name : "Gus Ayala"},
-                      { name : "Leighann Oros" },
-                      { name : "Mao Trombetta" },
+  factory.students = [{ name : "Gus Ayala", present: false },
+                      { name : "Leighann Oros", present: false },
+                      { name : "Mao Trombetta", present: false },
                       { name : "Nella Casale" },
                       { name : "Garret Pinnock" },
                       { name : "Buster Paolini" },
@@ -14,6 +14,17 @@ studentSignIn.factory('StudentsFactory', function StudentsFactory() {
                       { name : "Aiko Welte" },
                       { name : "Regan Mose" },
                       { name : "Margarite Fenlon" }];
+
+  // factory.checkPresent = function() {
+  //   if (factory.students.present == true) {
+  //     console.log(factory.students.present);
+  //     return true;
+  //   }
+  //   console.log(factory.students.present);
+  //   return false
+  //
+  //
+  // };
 
   return factory;
 });
