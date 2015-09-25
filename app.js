1fdbe5cd1bda17367ccd55ff8d7dc61accf030dc
Brian Borealis <brian.borealis@gmail.com>
2015-09-25 08:33:52 -0700

    add angular and ui-router files and add home.html partial.

---
+++ app.js
@@ -0,0 +1,8 @@
+var studentSignIn = angular.module('studentSignIn', ['ui.router']);
+
+studentSignIn.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url:"",
+    templateUrl: "partials/home.html"
+  });
+});
